b2e61611403789eb3884b1822dc92422da19aa8a
Musa Basili <musabasili035@gmail.com>
2026-08-03 16:25:06 +0300

    Only include static dir in STATICFILES_DIRS when it exists

--- iyunga_waste/settings.py
+++ iyunga_waste/settings.py
@@ -100,7 +100,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
